cb66baef04262be0d4f2e476cf64a407cb7eedfc
Matyas Albert Nagy <albertmatyi@gmail.com>
2014-09-20 17:47:50 +0200

    updated styles

--- client/views/layout/layout.js
+++ client/views/layout/layout.js
@@ -3,9 +3,9 @@ $(document).on('touchmove', function(e) {
 });
 
 $(document).ready(function($) {
-	// var url = 'http://192.168.21.119:8080/target/target-script-min.js#' + (navigator.userAgent.match(/iPad/i) !== null ? 'ipad' : 'normal');
-	// console.log(url);
-	// $('head').append('<script type="text/javascript" src="' + url + '"></script>');
+	var url = 'http://192.168.21.119:8080/target/target-script-min.js#' + (navigator.userAgent.match(/iPad/i) !== null ? 'ipad' : 'normal');
+	console.log(url);
+	$('head').append('<script type="text/javascript" src="' + url + '"></script>');
 });
 
 Template.layout.helpers({
