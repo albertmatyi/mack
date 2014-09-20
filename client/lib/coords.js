20175a40a85511248ff0f18e4eb3643af2bf3c11
Matyas Albert Nagy <albertmatyi@gmail.com>
2014-09-20 11:47:50 +0200

    removed console.log

--- client/lib/coords.js
+++ client/lib/coords.js
@@ -3,7 +3,6 @@ App.component('coords').expose({
 
 Meteor.startup(function () {
 	setInterval(function () {
-		console.log('set interval');
 		navigator.geolocation.getCurrentPosition(function(position) {
 			Session.set('coords', position.coords);
 			// console.log(navigator.platform, position.coords.latitude);
